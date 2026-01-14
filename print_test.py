095a20230047dd4dc4d1cb1ccaedb0669e1643dc
CatBrainJordanABaumann <bbaumann2194@floridapoly.edu>
2026-01-14 19:22:31 +0000

    Completed Code

---
+++ print_test.py
@@ -0,0 +1,7 @@
+my_name = "Jordan A. Baumann"
+print(f"I am {my_name}!")
+print("Use 4 spaces per indentation level")
+print("Python 3.0 first released in 2018")
+print("I hope to learn more about Linux as I know very little")
+print("I hope to learn new Python tools")
+print("I hope to learn cool new algorithms")
